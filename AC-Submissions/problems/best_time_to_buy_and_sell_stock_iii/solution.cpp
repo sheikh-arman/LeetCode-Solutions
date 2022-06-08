44c59e97b86f31d09201adf4892f9e082352a4c2
Sk Arman Hossain <arman.cse4.bu@gmail.com>
2022-06-08 12:27:01 +0000

    Sync LeetCode submission - Best Time to Buy and Sell Stock III (cpp)

---
+++ AC-Submissions/problems/best_time_to_buy_and_sell_stock_iii/solution.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+     int mem[100010][3][3],n;
+    vector<int>V;
+    int dp(int i,int status,int k){
+        if(i>=n){
+            return 0;
+        }
+        if(mem[i][k][status]!=-1){
+            return mem[i][k][status];
+        }
+        int ans=0;
+        if(status){
+            ans=dp(i+1,0,k)+V[i];
+        }
+        else{
+            if(k>0){
+                ans=dp(i+1,1,k-1)-V[i];
+            }
+        }
+        ans=max(ans,dp(i+1,status,k));
+        return mem[i][k][status]=ans;
+    }
+    int maxProfit(vector<int>& prices) {
+        n=prices.size();
+        V=prices;
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=2;j++){
+                for(int x=0;x<2;x++){
+                    mem[i][j][x]=-1;
+                }
+            }
+        }
+        int ans=dp(0,0,2);
+        return ans;
+    }
+};
