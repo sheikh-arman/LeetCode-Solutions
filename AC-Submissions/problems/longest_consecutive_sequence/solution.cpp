cc220952d384a2a61652022bdd3cb9e097b4e15f
Sk Arman Hossain <arman.cse4.bu@gmail.com>
2022-07-05 18:39:55 +0000

    Sync LeetCode submission - Longest Consecutive Sequence (cpp)

--- AC-Submissions/problems/longest_consecutive_sequence/solution.cpp
+++ AC-Submissions/problems/longest_consecutive_sequence/solution.cpp
@@ -1,42 +1,25 @@
 class Solution {
 public:
-    int mx;
-    map<int,int> rep,cnt;
-    int Find(int n){
-        return (rep[n]==n?n:rep[n]=Find(rep[n]));
-    }
-    void joint(int u,int v){
-        u=Find(u);
-        v=Find(v);
-        if(u!=v){
-            if(cnt[u]>=cnt[v]){
-                cnt[u]+=cnt[v];
-                mx=max(mx,cnt[u]);
-                rep[v]=u;
+    int longestConsecutive(vector<int>& nums) {
+       unordered_set<int>mp;
+        for(int i:nums){
+            mp.insert(i);
+        }
+        int ma=0,curr=0;
+        for(int i:mp){
+            if(mp.find(i-1)!=mp.end()){
+                continue;
             }
             else{
-                cnt[v]+=cnt[u];
-                mx=max(mx,cnt[v]);
-                rep[u]=v;
+                int cnt=1;
+                curr=i+1;
+                while(mp.find(curr)!=mp.end()){
+                    cnt++;
+                    curr++;
+                }
+                ma=max(ma,cnt);
             }
         }
-    }
-    int longestConsecutive(vector<int>& nums) {
-         rep.clear();cnt.clear();
-         mx=0;
-         if((int)nums.size())mx=1;
-         for(int i:nums){
-             rep[i]=i;
-             cnt[i]=1;
-         }   
-         for(int i:nums){
-             if(cnt[i-1]>0){
-                 joint(i-1,i);
-             }
-             if(cnt[i+1]>0){
-                 joint(i+1,i);
-             }
-         }
-         return mx;
+        return ma;
     }
 };
