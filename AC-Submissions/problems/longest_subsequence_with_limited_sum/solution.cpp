fc0e3dfb631def903c207276383e9c09c71b04f9
Sk Arman Hossain <arman.cse4.bu@gmail.com>
2022-12-25 06:22:59 +0000

    Sync LeetCode submission - Longest Subsequence With Limited Sum (cpp)

--- AC-Submissions/problems/longest_subsequence_with_limited_sum/solution.cpp
+++ AC-Submissions/problems/longest_subsequence_with_limited_sum/solution.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
+        vector<int>V;
         sort(nums.begin(),nums.end());
         int n=nums.size();
         for(int i=1;i<n;i++){
             nums[i]+=nums[i-1];
         }
-        int q=queries.size();
-        vector<int>ans;
-        for(int i=0;i<q;i++){
-            int pos=upper_bound(nums.begin(),nums.end(),queries[i])-nums.begin();
-            ans.push_back(pos);
+        for(int i:queries){
+            int pos=upper_bound(nums.begin(),nums.end(),i)-nums.begin();
+            V.push_back(pos);
         }
-        return ans;
+        return V;
     }
 };
