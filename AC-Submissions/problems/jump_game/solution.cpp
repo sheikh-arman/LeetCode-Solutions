32b2572b09bbf651292bb95f09d2646a56b0fb3e
Sk Arman Hossain <arman.cse4.bu@gmail.com>
2022-12-26 10:57:31 +0000

    Sync LeetCode submission - Jump Game (cpp)

--- AC-Submissions/problems/jump_game/solution.cpp
+++ AC-Submissions/problems/jump_game/solution.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int siz=nums.size();
-        int last=siz-1;
-        for(int i=siz-1;i>=0;i--){
-            if(nums[i]>=(last-i)){
-                last=i;
-            }
-        }
-        if(last==0){
-            return 1;
-        }
-        else{
-            return false;
-        }
+        int n=nums.size();
+        int cnt=-1;
+        int last=n-1;
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]){
+                if(last-i<=nums[i])
+                    {
+                    last=i;
+                    }
+
+                }
+
+         }
+        return last==0;
     }
 };
