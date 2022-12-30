b85af3770eda8cd1d5bbc96de2e96839d715042a
Sk Arman Hossain <arman.cse4.bu@gmail.com>
2022-12-30 16:57:04 +0000

    Sync LeetCode submission - All Paths From Source to Target (cpp)

--- AC-Submissions/problems/all_paths_from_source_to_target/solution.cpp
+++ AC-Submissions/problems/all_paths_from_source_to_target/solution.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
-   vector<vector<int>>V;
     int n;
-    void rec(int node,vector<int> path,vector<vector<int>>& graph){
+    vector<vector<int>>V;
+    vector<int>path;
+    void dfs(int node, int parent, vector<vector<int>>& graph){
         if(node==n-1){
-            path.push_back(node);
             V.push_back(path);
             return;
         }
-        path.push_back(node);
         for(int i:graph[node]){
-            rec(i,path,graph);
+            if(i!=parent){
+                path.push_back(i);
+                dfs(i, node,graph);
+                path.pop_back();
+            }
         }
     }
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
         n=graph.size();
-        vector<int>v;
-        rec(0,v,graph);
+        path.push_back(0);
+        dfs(0,-1,graph);
         return V;
     }
 };
