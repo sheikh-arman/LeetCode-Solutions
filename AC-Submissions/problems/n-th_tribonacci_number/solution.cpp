1a1f694d5e9ee5dd45f1e4387f5aef97315e172d
Sk Arman Hossain <arman.cse4.bu@gmail.com>
2023-01-30 12:15:30 +0000

    Sync LeetCode submission - N-th Tribonacci Number (cpp)

--- AC-Submissions/problems/n-th_tribonacci_number/solution.cpp
+++ AC-Submissions/problems/n-th_tribonacci_number/solution.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
+int dp[38];
+  int fib1(int n){
+      if(n==0)return dp[n]=0;
+      if(n==1 or n==2)return dp[n]=1;
+      
+     if(dp[n]!=-1)return dp[n];
+
+      return dp[n]=fib1(n-1)+fib1(n-2)+fib1(n-3);
+  }
     int tribonacci(int n) {
-        int dp[n+5];
-        dp[0]=0;
-        dp[1]=1;
-        dp[2]=1;
-        for(int i=3;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
-        }
-        return dp[n];
+        memset(dp,-1,sizeof(dp));
+        return fib1(n);
     }
 };
