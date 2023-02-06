7e549aaa7fe8470ea7a74e5ab0985a010d9c5db5
Sk Arman Hossain <arman.cse4.bu@gmail.com>
2023-02-06 13:52:47 +0000

    Sync LeetCode submission - Shuffle the Array (cpp)

--- AC-Submissions/problems/shuffle_the_array/solution.cpp
+++ AC-Submissions/problems/shuffle_the_array/solution.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
         vector<int>V;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i+=1){
             V.push_back(nums[i]);
             V.push_back(nums[i+n]);
         }
