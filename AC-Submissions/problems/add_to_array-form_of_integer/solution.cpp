bf9a38a693b7b210cd32ff7a3614297023e5d5bc
Sk Arman Hossain <arman.cse4.bu@gmail.com>
2023-02-15 08:38:56 +0000

    Sync LeetCode submission - Add to Array-Form of Integer (cpp)

--- AC-Submissions/problems/add_to_array-form_of_integer/solution.cpp
+++ AC-Submissions/problems/add_to_array-form_of_integer/solution.cpp
@@ -1,20 +1,37 @@
 class Solution {
 public:
     vector<int> addToArrayForm(vector<int>& num, int k) {
-        int carry=k;
-        int n=num.size();
-        vector<int>V;
-        for(int i=n-1;i>=0;i--){
-            int number=num[i];
-            number+=carry;carry=0;
-            V.push_back(number%10);
-            carry=number/10;
+        reverse(num.begin(),num.end());
+        int n=num.size(),i=0,carry=0;
+        while(k){
+            carry+=k%10;
+            if(i<n){
+                carry+=num[i];
+                num[i]=carry%10;
+            }
+            else{
+                num.push_back(carry%10);
+            }
+            carry/=10;
+            k/=10;
+            i++;
         }
-        while(carry){
-            V.push_back(carry%10);
+        while(i<n){
+            carry+=num[i];
+            num[i]=carry%10;    
+            i++;
             carry/=10;
         }
-        reverse(V.begin(),V.end());
-        return V;
+        if(carry){
+            if(i<n){
+            carry+=num[i];
+            num[i]=carry%10;
+        }
+        else{
+            num.push_back(carry%10);
+        }
+        }
+        reverse(num.begin(),num.end());
+        return num;
     }
 };
