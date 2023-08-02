3555bf5b6527d1c3449663136639b130ba88b3d0
Sk Arman Hossain <arman.cse4.bu@gmail.com>
2023-08-02 14:23:48 +0000

    Sync LeetCode submission - Permutations (cpp)

--- AC-Submissions/problems/permutations/solution.cpp
+++ AC-Submissions/problems/permutations/solution.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
-    vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>>V;
-        sort(nums.begin(),nums.end());
-        do{
-            vector<int>tm;
-            for(int i:nums){
-                tm.push_back(i);
-            }
-            V.push_back(tm);
+    vector<vector<int>>ans;
+    void help(int mask,vector<int>&v,vector<int>&temp){
+        if(mask==0){
+            ans.push_back(temp);
+            return;
         }
-        while(next_permutation(nums.begin(),nums.end()));
-        return V;
+        for(int i=0;i<v.size();i++){
+            if(mask & (1<<i)){
+                temp.push_back(v[i]);
+                help(mask ^ (1<<i), v,temp);
+                temp.pop_back();
+            }
+        }
+    }
+    vector<vector<int>> permute(vector<int>& v) {
+        vector<int>temp;
+        help((1<<v.size())-1,v,temp);
+        return ans;
     }
 };
