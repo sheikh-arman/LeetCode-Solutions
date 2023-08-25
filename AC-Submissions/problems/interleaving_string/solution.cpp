0f7dc12232c13d0598290ec32c59c3974b4785aa
Sk Arman Hossain <arman.cse4.bu@gmail.com>
2023-08-25 16:03:07 +0000

    Sync LeetCode submission - Interleaving String (cpp)

--- AC-Submissions/problems/interleaving_string/solution.cpp
+++ AC-Submissions/problems/interleaving_string/solution.cpp
@@ -1,39 +1,22 @@
 class Solution {
 public:
-    int mem[110][110];
-    int dp(int i,int j,string s1,string s2,string s3){
-        if(i<s1.size()&&j<s2.size()){
-            if(s3[i+j]!=s1[i]&&s3[i+j]!=s2[j]){
-              return 0;
-            }
+    bool c(int i1,int i2,int i3,string &s1,string &s2,string &s3,
+    vector<vector<vector<int>>>&dp){
+        if(i3==s3.size()){
+            if(i2==s2.size() && i1==s1.size())return true;
+            else return false;
         }
-        if(i>=s1.size()&&j>=s2.size()){
-            return 1;
+        char c1='*',c2='*';
+        if(dp[i1][i2][i3]!=-1)return dp[i1][i2][i3];
+        if(i1<s1.size())c1=s1[i1];
+        if(i2<s2.size())c2=s2[i2];
+        bool ans=false;
+        if(c1==s3[i3])ans |= c(i1+1,i2,i3+1,s1,s2,s3,dp);
+        if(c2==s3[i3])ans |= c(i1,i2+1,i3+1,s1,s2,s3,dp);
+        return dp[i1][i2][i3]=ans;
         }
-        if(mem[i][j]!=-1){
-            return mem[i][j];
-        }
-        int ans=0;
-        if(i<s1.size()&&s3[i+j]==s1[i]){
-            ans=dp(i+1,j,s1,s2,s3);
-        }
-        if(j<s2.size()&&s3[i+j]==s2[j]){
-            ans|=dp(i,j+1,s1,s2,s3);
-        }
-        return mem[i][j]=ans;
-    }
     bool isInterleave(string s1, string s2, string s3) {
-        int n=s1.size();
-        int m=s2.size();
-        if(n+m!=s3.size()){
-            return 0;
-        }
-        for(int i=0;i<=n;i++){
-            for(int j=0;j<=m;j++){
-                mem[i][j]=-1;
-            }
-        }
-        int ans=dp(0,0,s1,s2,s3);
-        return (bool)ans;
+        vector<vector<vector<int>>>dp(s1.size()+1,vector<vector<int>>(s2.size()+1,vector<int>(s3.size(),-1)));
+        return c(0,0,0,s1,s2,s3,dp);
     }
 };
