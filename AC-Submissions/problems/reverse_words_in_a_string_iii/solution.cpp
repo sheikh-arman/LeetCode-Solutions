919fc2efc7a18d4365f126f7f3693ca5e46a506b
Sk Arman Hossain <arman.cse4.bu@gmail.com>
2023-10-01 08:07:31 +0000

    Sync LeetCode submission - Reverse Words in a String III (cpp)

--- AC-Submissions/problems/reverse_words_in_a_string_iii/solution.cpp
+++ AC-Submissions/problems/reverse_words_in_a_string_iii/solution.cpp
@@ -1,18 +1,31 @@
 class Solution {
 public:
-    string reverseWords(string s) {
-        int i=0;
-        for(int j=0;j<s.length();j++)
-        {
-            if(s[j]==' ')
-            {
-                reverse(s.begin()+i,s.begin()+j);
-                i=j+1;
-            }
+    string rev(string s){
+         int n=s.size();
+        for(int i=0;i<n/2;i++){
+            char ch=s[i];
+            s[i]=s[n-i-1];
+            s[n-i-1]=ch;
         }
-        reverse(s.begin()+i,s.end());
         return s;
     }
+    string reverseWords(string s) {
+        string res;
+        int n=s.size();
+        string tm;
+        for(int i=0;i<n;i++){
+            if(s[i]!=' '){
+                tm+=s[i];
+            }
+            else{
+                tm=rev(tm);
+                res+=tm;
+                res+=" ";
+                tm="";
+            }
+        }
+         tm=rev(tm);
+         res+=tm;
+        return res;
+    }   
 };
-
-
